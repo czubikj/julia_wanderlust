c9e42e5307a89ef074216bc9625818f9620828a5
Julia Czubik <113396761+czubikj@users.noreply.github.com>
2023-06-20 16:26:13 -0400

    Customized the admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'created',
+        'updated',
+    )
+
+# Register the `Post` model
+admin.site.register(models.Post)
