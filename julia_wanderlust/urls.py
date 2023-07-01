1d36b9c206bd5761d66462923cb871c55bcc6827
Julia Czubik <113396761+czubikj@users.noreply.github.com>
2023-06-30 21:31:11 -0400

    Base Template w/ CSS Base

--- julia_wanderlust/urls.py
+++ julia_wanderlust/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from . import views   # Import the views module
+#from . import views   # Import the views module
+from blog import views #Import the blog views
 
 urlpatterns = [
-    path('', views.index),  # Add our index view to the URL patterns
+    #path('', views.index),  # Add our index view to the URL patterns/not required, will add later??
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),  # Set root to home view
 ]
