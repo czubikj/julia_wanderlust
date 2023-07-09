ccfaa62ac610b2dbb503683bcd3047b4af65a493
Julia Czubik <113396761+czubikj@users.noreply.github.com>
2023-07-09 19:27:16 -0400

    Back to the Basics!
    Removed all changes I could since last git push which weren't working. Minor updates to content shown.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def home(request):
     """
     The Blog homepage
     """
-    return render(request, 'blog/home.html')
+    # Get last 3 posts
+    latest_posts = models.Post.objects.published().order_by('-published')[:3]
+
+    context = {
+        'latest_posts': latest_posts,
+    }
+
+    return render(request, 'blog/home.html', context)
