5d8a8d579e0b0ba3cab6831e754be33e4b338f8e
Julia Czubik <113396761+czubikj@users.noreply.github.com>
2023-08-15 12:28:54 -0400

    Updated stuff

--- julia_wanderlust/settings.py
+++ julia_wanderlust/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'julia_wanderlust.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'kris', #or the username you picked when creating the Postgres admin
+        'PASSWORD': '<krisczubik>',
+        'HOST': '<dash-of-wanderlust.postgres.database.azure.com>', #should end with postgres.database.azure.com
+        'PORT': '5432',
     }
 }
 
